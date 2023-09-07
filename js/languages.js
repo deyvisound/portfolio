eeacbe7ffd950105c31662d164762e334637d504
Deyvison Estevam <deyvisonestevam@gmail.com>
2023-09-07 17:51:52 -0300

    Atualizacao de Portifolio

--- js/languages.js
+++ js/languages.js
@@ -24,50 +24,62 @@ language["pt-br"]["skills"] = "Habilidades";
 language["pt-br"]["contact"] = "Contato";
 
 language["pt-br"]["about_text1"] = 
-"Sou Deyvison Rodrigo B. Estevam, nascido em 23 de novembro de 1991. " +
-"Minha vida profissional começou bem cedo por volta dos 15 anos de idade, porém não na área de TI. " + 
-"Trabalhei em supermercados, lojas e até com entregas.";
+"Queria começar esta conversa dizendo que minha vida profissional começou bem cedo, por volta dos 15 anos de idade, porém não na área de TI. " + 
+"Trabalhei em supermercados, lojas e até com entregas. Mas sempre conciliei com os estudos, pois sempre almejei algo maior para minha vida.";
 
 language["pt-br"]["about_text2"] = 
-"No ano de 2011 quando entrei na Universidade(UFRN), me deparei com este mundo do qual adoro " +
-"fazer parte. A sensação de programar a máquina,  \"ordenar\" com que ela fizesse algo, " + 
-"era e ainda é maravilhoso. ";
+"Em 2011, quando entrei na Universidade(UFRN), me deparei com este mundo o qual me sinto lisongeado por fazer parte. " +
+"A sensação de programar a máquina para automatizar tarefas era e ainda algo maravilhoso aos meus olhos. ";
 
 language["pt-br"]["exp_text1"] =
-"Minha experiência com TI começou em 2011 na universidade, existiam disciplinas de lógica e linguagens " + 
-"de programação. No final do segundo semestre consegui uma bolsa de tutor na qual eu era responsável por " +
-"ajudar alunos ingressantes em disciplinas de alto índice de reprovação como matemática, fisíca e linguagem " + 
-" de programação.";
+"Minha experiência com TI começou já em 2011 na universidade, pois existiam disciplinas de lógica e linguagens de programação as quais eu tinha certa facilidade. "
+"Sendo assim, consegui uma bolsa como tutor destas disciplinas. A minha atribuição principal era ajudar os alunos ingressantes com estas matérias, afim de reduzir os indices de reprovação nas mesmas. " + 
+"Passei 2 anos nesta atividade.";
 
 language["pt-br"]["exp_text2"] =
-"Após 2 anos, tempo máximo de permanência na bolsa, consegui um estágio na superintendência do Banco do Brasil. " + 
-"Lá trabalhei com Java Web(JSP, Hibernate, Mysql) e desenvolvimento de aplicativos Desktop com Visual Basic for Applications.";
+"Do início de 2013 a 2015, fui estagiário na superintendência do Banco do Brasil do RN. " + 
+"Nesta experiência trabalhei principalmente com <b>Java Web(JSP, Hibernate)</b>, <b>Javascript(Ajax e Jquery)</b>, Banco de dados <b>Mysql</b> e desenvolvimento de apps Desktop com <b>Visual Basic</b>.";
 
 language["pt-br"]["exp_text3"] = 
-"Então, após o término do contrato do estágio, em fevereiro de 2015, fui selecionado para vaga de programador jr " + 
-"em um provedor de internet da região. A linguagem principal era o PHP, porém participei brevemente em projetos adjacentes que " + 
-"utilizavam outras tecnologias como c++ e python. Desenvovi sistemas(ERP, CRM) e sites, após 2 anos fui para o cargo de " + 
-"analista de desenvolvimento de sistemas. Passei mais 10 meses na empresa, contando um total de 2 anos " + 
-" e 10 meses de experiencia com tecnologias PHP.";
+"Em fevereiro de 2015, consegui meu primeiro emprego CLT como programador jr. " + 
+"Era em um provedor de internet que atuava principalmente no interior do estado. A principal tecnologia utilizada era o <b>PHP(Laravel 5.0, Zend 2.0 e ORM Doctrine 2)</b>. " +
+"Ainda na mesma empresa, fui alocado em outros projetos que utilizavam outras tecnologias como <b>C++</b> e <b>python</b>.<br/> " +
+"Desenvovi sistemas(ERP, CRM) e sites para o público externo. " + 
+"Não demorou muito para que fosse promovido a Analista de Desenvolvimento de Sistemas. " + 
+"<br/>Foram quase 3 anos de experiência nesta empresa, período bastante enriquecedor para meu currículo.";
 
 language["pt-br"]["exp_text4"] = 
-"Hoje trabalho como desenvolvedor de sistemas na eSig Software e Consultoria, empresa especializada em sistemas de " + 
-"gestão na área de educação e saúde. Comecei em Fevereiro de 2018 e estou até o presente momento. " +
-"Aqui trabalho com diversas tecnologias do mundo Java, dentre elas posso citar Hibernate, JSF, RichFaces... Banco de dados Postgresql. " + 
-"Foi aqui que aprendi que um bom profissional na área de T.I. não é composto somente de habilidades técnicas, é necessários " + 
-"também ter uma base sólida de conhecimento em  processos, negócios e metodologias... ";
+"Entre 2018 e 2021, fui contratado por uma fábrica de software especializada em sistemas de gestão na área de educação e saúde. " + 
+"Após 3 longos anos, voltei a trabalhar com meu querido <b>Java</b> rs, desta vez com tecnologias mais robustas como <b>(Hibernate, JSF, RichFaces, PrimeFaces, Struct 2 e SpringMVC)</b>. " +
+"Atuei também com banco de dados <b>Postgresql</b> e com <b>TypeScript</b> no frontend. Servidor de Aplicação <b>JBOSS</b>." + 
+"Foi aqui que aprendi que um bom profissional na área de TI não é composto somente de habilidades técnicas, também é necessário ter uma base sólida de conhecimento em processos, negócio e metodologias( SCRUM, KANBAN e etc ). " + 
+"<br/>Esta experiência durou por volta de 3 anos e 3 meses. ";
+
+language["pt-br"]["exp_text5"] = 
+"Em 2021 fui chamado para trabalhar remotamente para uma consultoria de Brasilia(DF), a qual possuia um grande cliente do setor financeiro. " + 
+"As principais tecnologias eram <b>JAVA</b> no backend e, uma novidade para mim, Apache FLEX no frontend. " +
+"Utilizávamos tecnologias da IBM, como o banco de dados <b>DB2</b> e o Servidor de Aplicação <b>WebSphere Application Server 9</b>." 
+"<br/>Foi uma experiência rápida, por volta de 4 meses, porém não menos enriquecedora que as outras. ";
+
+language["pt-br"]["exp_text6"] = 
+"No fim de 2021 fui convidado para trabalhar em outra consultoria, desta vez uma de Belo Horizonte/MG. <br/>" + 
+"O <b>primeiro</b> projeto que atuei era um <i>upgrade</i> das tecnologias de um sistema. Seria migrado de <b>JAVA(JSF e PRIMEFACES)</b> para <b>JAVA(SPRINGBOOT e THYMELEAF)</b>. " +
+"O Servidor de aplicação era o <b>JBOSS 5</b>. Já o banco de dados( Caché Database ) e o broker utilizado para mensageria eram da empresa InterSystems. <br/>" + 
+"O <b>segundo</b> projeto que atuei, é um sistema robusto responsável por grande parte das operações de uma gigante das telecomunicações. " + 
+"A principal tecnologia utilizada neste projeto é o JAVA com Hibernate para mapeamento de dados. O banco de dado é o <b>OracleDb</b> e o servidor de aplicação é o <b>Weblogic 11</b>, também utilizado para mensageria. "
+"<br/>Estou nesta experiência até o presente momento. ";
+
 
 language["pt-br"]["sch_text1"] = 
-"Todo meu ensino fundamental e médio fora em escola pública. Durante a adolescência fiz cursos técnicos " +
+"Todo meu ensino fundamental e médio foi em escola pública. Durante a adolescência fiz cursos técnicos " +
 "e profissionalizantes na área de <u>gestão pessoal</u>, <u>informática</u> e <u>idiomas</u>.<br/>";
 
 language["pt-br"]["sch_text2"] = 
-"Em 2010 fiz vestibular para administração, felizmente não passei rs, no ano seguinte(2011) " +
-"tentei Ciências e Tecnologia, passei e me formei em 2015. <br/>" + 
-"Escolhi Engenharia de Telecomunicações como curso de segundo ciclo porém, percebi depois que " +
-"não era o que eu queria e acabei não concluíndo o curso. <br/>" +
-"Procuro sempre está me especializando, por isto continuo fazendo cursos em plataformas online " +
-"como Udemy, AlgaWorks...";
+"Em 2010 fiz vestibular para administração, (in)felizmente não passei, no ano seguinte(2011) já no ano seguinte tentei Ciências e Tecnologia, passei e me formei em 2015. <br/>" + 
+"Escolhi Engenharia de Telecomunicações como curso de segundo ciclo porém, percebi depois que não era o que eu queria e acabei não concluíndo o curso. <br/>" +
+"Procuro sempre está me especializando, por isto continuo fazendo cursos em plataformas online e afins. " +
+"Fique a vontade para me contactar através e minhas redes sociais( canto superior esquerdo desta página ). " + 
+"E muito obrigado por ter lido até aqui :) ";
 
 
 
